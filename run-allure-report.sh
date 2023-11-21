7370343ea0cd729bb9492616bd52dbce00a7b90f
ibrahimazeez <82159486+ibrahimazeez@users.noreply.github.com>
2023-11-21 01:38:45 +0100

    Delete run-allure-report.sh

--- run-allure-report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run -v "$(pwd):/workspace" simple-elf/allure-report-action@v1.7 \
-  allure_results=/workspace/allure-results \
-  gh_pages=/workspace/gh-pages \
-  allure_report=/workspace/allure-report \
-  allure_history=/workspace/allure-history
